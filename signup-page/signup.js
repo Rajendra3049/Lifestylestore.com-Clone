9f84bcba9e56d08a1296accf146daab59b7b07fa
Prabhat <prabhatjain19950@gmail.com>
2022-11-10 19:44:07 +0530

    updating file

--- signup-page/signup.js
+++ signup-page/signup.js
@@ -1,1 +1,22 @@
-console.log('love')
+function setFormMessage(formElement,type,message){
+    
+}
+
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const loginForm = document.querySelector('#login');
+    const createAccountForm = document.querySelector('#createAccount');
+
+    document.querySelector('#linkCreateAccount').addEventListener("click", e => {
+       e.preventDefault();
+       loginForm.classList.add('form--hidden');
+       createAccountForm.classList.remove('form--hidden');
+    });
+
+    document.querySelector('#linkLogin').addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove('form--hidden');
+        createAccountForm.classList.add('form--hidden');
+     });
+});
