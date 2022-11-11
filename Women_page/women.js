2135c67027ae27c2fd9b57fd93773215a0aa0e7e
Rajendra3049 <Rajendrapatel3049@gmail.com>
2022-11-11 19:29:32 +0530

    day 5 half task complete by rajendra

--- Women_page/women.js
+++ Women_page/women.js
@@ -1,10 +1,15 @@
-// ---------------------------------------impot--------------------------------------------
+// // ---------------------------------------impot--------------------------------------------
+// import {navbar} from "../components2/nav.js"
+// import {footer} from "../components2/footer.js"
+// // ------------------------------------select all div--------------------------------------
+// let nav_div=document.getElementById("navbar_RP")
+// nav_div.innerHTML=navbar()
 
-// ------------------------------------select all div--------------------------------------
-
+// let footer_div=document.getElementById("Footer_RP")
+// footer_div.innerHTML=footer()
 
 // -----------------------------------------carousel----------------------------------------
-let carousel_array=["https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktop-1-07Nov2022.gif","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner5-desktop-Women-07Nov2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktopp-2-07Nov2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktopp-4-07Nov2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner6-desktop-Women-08Nov2022.gif"]
+let carousel_array=["https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktop-1-07Nov2022.gif","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktopp-2-07Nov2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/HP_LS_Women_Desktopp-4-07Nov2022.jpg","https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Banner6-desktop-Women-08Nov2022.gif"]
 
 const carousel=()=>{
     let slideshow_div=document.getElementById("carousel")
@@ -22,7 +27,7 @@ const carousel=()=>{
         slideshow_div.append(image)
 
         i++
-    },6000)
+    },5000)
 }
 
 carousel()
